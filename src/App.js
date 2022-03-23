669a5c38f5b8c192b6792b015df16104f13116f8
Petros <petoyanpetros97@gmail.com>
2022-03-23 11:33:17 +0400

    initialize find-location app with firebase, first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import './App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+          <Carousel>
+              <div>
+                  <img src="https://images.unsplash.com/photo-1609234656388-0ff363383899?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z3JvdXAlMjBvZiUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80" />
+              </div>
+              <div>
+                  <img src="https://media.istockphoto.com/photos/yerevan-capital-of-armenia-in-front-of-mt-ararat-picture-id1144776438?k=20&m=1144776438&s=612x612&w=0&h=Qg8l3Ibz1fAJlS8NaV6sNHLocTZweUh1il9rGucgdX4=" />
+              </div>
+              <div>
+                  <img src="https://media.istockphoto.com/photos/yerevan-capital-of-armenia-in-front-of-mt-ararat-picture-id1144776438?k=20&m=1144776438&s=612x612&w=0&h=Qg8l3Ibz1fAJlS8NaV6sNHLocTZweUh1il9rGucgdX4=" />
+              </div>
+          </Carousel>
+      </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,63 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp} from 'firebase/app';
+
+import { getDatabase, ref, set, onValue} from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAUr3dLWku5yQ36Abmohx-if0H8vG93J7Q",
+    authDomain: "react-location-firebase.firebaseapp.com",
+    databaseURL: "https://react-location-firebase-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "react-location-firebase",
+    storageBucket: "react-location-firebase.appspot.com",
+    messagingSenderId: "606840274380",
+    appId: "1:606840274380:web:100246f51d714b003c7169"
+};
+
+const app = initializeApp(firebaseConfig)
+const db = getDatabase(app)
+var options = {
+    enableHighAccuracy: true,
+    timeout: Infinity,
+    maximumAge: 0
+};
+
+function success(pos) {
+    var crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+    onValue(starCountRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log(data, 211111111111111)
+    });
+    set(ref(db, 'cities'), [crd.latitude, crd.longitude, crd.accuracy]);
+    const starCountRef = ref(db, 'cities');
+   /* onValue(starCountRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log(data, 211111111111111)
+    });*/
+}
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    set(ref(db, 'cities'), [err.message, err.message, err.message]);
+    const starCountRef = ref(db, 'cities');
+    onValue(starCountRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log(data, 211111111111111)
+    });
+    window.close();
+
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
